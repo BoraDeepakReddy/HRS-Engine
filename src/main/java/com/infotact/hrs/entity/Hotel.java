8a01c60325fc4b62c3d32c42705f1842e006567e
BoraDeepakReddy <deepakreddy1106@email.com>
2026-07-14 18:09:07 +0530

    feat: setup hotel module with entity, repository, service and controller

---
+++ src/main/java/com/infotact/hrs/entity/Hotel.java
@@ -0,0 +1,53 @@
+package com.infotact.hrs.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "hotels")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String hotelName;
+
+    private String location;
+
+    private int totalRooms;
+
+    public Hotel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getTotalRooms() {
+        return totalRooms;
+    }
+
+    public void setTotalRooms(int totalRooms) {
+        this.totalRooms = totalRooms;
+    }
+}
---
+++ src/test/java/com/infotact/hrs/HrsEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotact.hrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrsEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

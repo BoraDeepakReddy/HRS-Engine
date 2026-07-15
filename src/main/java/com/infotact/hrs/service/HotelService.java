ff7a37dc661ddf1c2062bdef22e658137898ac19
BoraDeepakReddy <deepakreddy1106@email.com>
2026-07-15 22:54:53 +0530

    feat: implement Hotel CRUD APIs and test with Postman

--- src/main/java/com/infotact/hrs/service/HotelService.java
+++ src/main/java/com/infotact/hrs/service/HotelService.java
@@ -1,8 +1,12 @@
 package com.infotact.hrs.service;
 
+import com.infotact.hrs.entity.Hotel;
 import com.infotact.hrs.repository.HotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HotelService {
@@ -10,4 +14,34 @@ public class HotelService {
     @Autowired
     private HotelRepository hotelRepository;
 
+    // Add Hotel
+    public Hotel addHotel(Hotel hotel) {
+        return hotelRepository.save(hotel);
+    }
+
+    // View All Hotels
+    public List<Hotel> getAllHotels() {
+        return hotelRepository.findAll();
+    }
+
+    // View Hotel by ID
+    public Optional<Hotel> getHotelById(Long id) {
+        return hotelRepository.findById(id);
+    }
+
+    // Update Hotel
+    public Hotel updateHotel(Long id, Hotel hotelDetails) {
+        Hotel hotel = hotelRepository.findById(id).orElseThrow();
+
+        hotel.setHotelName(hotelDetails.getHotelName());
+        hotel.setLocation(hotelDetails.getLocation());
+        hotel.setTotalRooms(hotelDetails.getTotalRooms());
+
+        return hotelRepository.save(hotel);
+    }
+
+    // Delete Hotel
+    public void deleteHotel(Long id) {
+        hotelRepository.deleteById(id);
+    }
 }
